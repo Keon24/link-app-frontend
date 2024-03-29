b8ff72c93873c9dfb29291f0a85dce266da31701
Keon24 <keonpowell95@gmail.com>
2024-03-29 10:44:58 -0700

    styled the login and register

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import RegisterPage from "./pages/RegisterPage"
+import LoginPage from "./pages/LoginPage"
+import Links from "./components/Links"
+
+function App() {
+  return (
+   <Router>
+    <Routes>
+      <Route path="/" element={ <LoginPage />}/> 
+      <Route path="login" element={ <LoginPage />}/> 
+      <Route path="/login" element={ <LoginPage />}/> 
+      <Route path="/register" element={ <RegisterPage />}/> 
+      <Route path="links" element={ <Links />}/> 
+    </Routes>
+   </Router>
+  );
+}
+
+export default App;
